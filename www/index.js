579fac1b035a72cad83d1d139e0efd32c39286ca
iiPython <ben@iipython.cf>
2022-04-06 08:53:17 -0500

    Implement date syncing

--- www/index.js
+++ www/index.js
@@ -15,10 +15,13 @@ const app = express()
 const dtf = new Intl.DateTimeFormat("en-US");
 
 // Handler functions
+function formatDate(d) {
+    return dtf.format(d).replace(/\//g, "-");
+}
 function getPastWeek() {
     let week = {}, now = new Date();
     for (let i = 0; i < 5; i++) {
-        let date = dtf.format(new Date(now - (i * 86400000))).replace(/\//g, "-");
+        let date = formatDate(new Date(now - (i * 86400000)));
         let fpath = path.resolve(config["warehouse.dataLocation"], `${date}.json`);
         if (fs.existsSync(fpath)) week[date] = md5File.sync(fpath);
     }
@@ -28,6 +31,16 @@ function getPastWeek() {
 // Routes
 app.get("/sync/status", (req, res) => {
     return res.send(getPastWeek());
+})
+app.get("/sync/:date", (req, res) => {
+    try {
+        let date = formatDate(new Date(req.params.date));
+        let fpath = path.resolve(config["warehouse.dataLocation"], `${date}.json`);
+        if (!fs.existsSync(fpath)) return res.sendStatus(404);
+        return res.send(fs.readFileSync(fpath));
+    } catch {
+        return res.sendStatus(400);
+    }
 })
 
 // Start app
